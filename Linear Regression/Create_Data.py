276806eb5fb51254f9c8b4adf844cfd1ee224fc6
khooinguyeen <khoinguyenmai17102005@gmail.com>
2023-05-10 09:11:22 +0700

    notes & explainations

--- Linear Regression/Create_Data.py
+++ Linear Regression/Create_Data.py
@@ -2,11 +2,31 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-numOfPoint = 30
-noise = np.random.normal(0, 1, numOfPoint).reshape(-1, 1)
+numOfPoint = 30 # đặt số điểm dữ liệu là 30
+
+noise = np.random.normal(0, 1, numOfPoint).reshape(-1, 1) 
+# tạo nhiễu ngẫu nhiên bằng normal distribution với trung bình 0 và độ lệch chuẩn 1
+# mảng được thay đổi kích thước để trở thành vector có kích thước (numOfPoint, 1)
+
 x = np.linspace(30, 100, numOfPoint).reshape(-1, 1)
-N = x.shape[0]
-y = 15*x + 8 + 20*noise
+# tạo ra một mảng x chứa numOfPoint giá trị cách đều nhau từ 30 đến 100 (bao gồm cả hai đầu). 
+# Hàm np.linspace được sử dụng để tạo ra các giá trị này.
+# Mảng kết quả được thay đổi kích thước để trở thành một vector cột có kích thước (numOfPoint, 1).
+
+N = x.shape[0] # gán số hàng trong mảng x cho biến N. Trong trường hợp này, N sẽ bằng numOfPoint.
+
+y = 15*x + 8 + 20*noise 
+# tính toán các giá trị y tương ứng dựa trên một phương trình tuyến tính. 
+# Mảng y kết quả sẽ có cùng kích thước với x.
+
 plt.scatter(x, y)
-np.savetxt('Linear Regression/test_data.csv', (x, y))
+# tạo ra một biểu đồ phân tán bằng cách sử dụng hàm scatter của Matplotlib. 
+# Nó nhận mảng x và y làm đầu vào và vẽ các điểm trên đồ thị. 
+# Mỗi cặp (x, y) đại diện cho một điểm trên biểu đồ phân tán.
+
 plt.show()
+
+data = np.concatenate((x, y), axis=1)  # Ghép mảng x và y thành một mảng 2D
+
+np.savetxt('data.csv', data, delimiter=',') 
+# Lưu mảng data vào file data.csv với dấu phẩy (,) làm dấu phân cách 
--- Linear Regression/Linear_Regression.py
+++ Linear Regression/Linear_Regression.py
@@ -10,34 +10,69 @@ import matplotlib.pyplot as plt
 # y = 15*x + 8 + 20*noise
 # plt.scatter(x, y)
 
-data = pd.read_csv('Linear Regression/data_linear.csv').values
-N = data.shape[0]
+data = pd.read_csv('Linear Regression/data_linear.csv').values 
+# Đọc nội dung của tệp CSV có tên là "data_linear.csv" bằng hàm read_csv của pandas. 
+# Thuộc tính values được sử dụng để trích xuất dữ liệu dưới dạng một mảng NumPy và gán vào biến data.
+
+N = data.shape[0] # số hàng trong mảng data, đại diện cho số điểm dữ liệu (30)
 x = data[:, 0].reshape(-1, 1)
-y = data[:, 0].reshape(-1, 1)
-plt.scatter(x, y)
-plt.xlabel('mét vuông')
-plt.ylabel('giá')
+# Trích xuất các giá trị từ cột đầu tiên của mảng data và 
+# điều chỉnh kích thước thành một vector cột bằng reshape. 
+# Vector cột được gán vào biến x.
 
-x = np.hstack((np.ones((N, 1)), x))
+y = data[:, 1].reshape(-1, 1) # như code trên nma là cột thứ 2
+
+plt.scatter(x, y) # tạo biểu đồ phân tán
+
+plt.xlabel('mét vuông') # đặt nhãn cho trục x
+plt.ylabel('giá') # đặt nhãn cho trục y
+
+x = np.hstack((np.ones((N, 1)), x)) 
+# Thêm một cột chứa các giá trị 1 vào mảng x bằng cách sử dụng hàm np.hstack, 
+# mảng x được sửa đổi. 
+# Thao tác này thường được thực hiện để tính đến thuật ngữ bias trong các mô hình hồi quy tuyến tính.
+
+
 w = np.array([0.,1.]).reshape(-1, 1)
+# Tạo một vector trọng số ban đầu w dưới dạng một vector cột, được khởi tạo với các giá trị [0, 1].
 
-numOfIteration = 100
-cost = np.zeros((numOfIteration, 1))
-learning_rate = 0.000001
-for i in range(1, numOfIteration):
-    r = np.dot(x, w) - y
+# => Tóm tắt, đoạn code đọc dữ liệu từ tệp CSV, tạo một biểu đồ phân tán các điểm dữ liệu, 
+# thêm một cột giá trị 1 vào mảng đầu vào và khởi tạo một vector trọng số cho mô hình hồi quy tuyến tính
+
+numOfIteration = 100 # số lần lặp
+cost = np.zeros((numOfIteration, 1)) 
+# tạo mảng cost với kích thước (100, 1) gồm toàn số 0, sử dụng để lưu giá trị của cost function khi train
+
+learning_rate = 0.000001 # set learning rate, dùng để điều chỉnh tốc độ cập nhật của weights
+
+# Gradient descent
+for i in range(1, numOfIteration): # vòng lặp huấn luyện
+    r = np.dot(x, w) - y 
+    # Biến r được tính toán bằng hiệu của tích vô hướng giữa x và w và y. Đây là sai số dự đoán.
+
     cost[i] = 0.5*np.sum(r*r)
-    w[0] -= learning_rate*np.sum(r)
+    # Gán giá trị của hàm chi phí (cost function) cho phần tử thứ i trong mảng cost. 
+    # Hàm chi phí được tính bằng nửa tổng bình phương của r, đại diện cho bình phương sai số.
+
+    w[0] -= learning_rate*np.sum(r) 
+    # Cập nhật trọng số đầu tiên (w[0]) bằng cách trừ đi learning_rate rồi nhân với tổng của r.
     # correct the shape dimension
-    w[1] -= learning_rate*np.sum(np.multiply(r, x[:, 1].reshape(-1, 1)))
+    w[1] -= learning_rate*np.sum(np.multiply(r, x[:, 1].reshape(-1, 1))) 
+    # Cập nhật trọng số thứ hai (w[1]) bằng cách trừ learning_rate nhân với tổng của tích element-wise giữa r và cột thứ hai của x.
     print(cost[i])
 
-predict = np.dot(x, w)
+predict = np.dot(x, w) 
+# Dự đoán đầu ra predict bằng tích vô hướng giữa x và w. 
+# Đây là dự đoán giá trị đầu ra dựa trên mô hình hồi quy tuyến tính đã huấn luyện.
+
 plt.plot((x[0][1], x[N-1][1]),(predict[0], predict[N-1]), 'r')
+# Vẽ một đường thẳng trên biểu đồ, nối các điểm (x[0][1], predict[0]) và (x[N-1][1], predict[N-1]), 
+# được biểu thị bằng màu đỏ ( 'r').
+
 plt.show()
 
 x1 = 50 
-y1 = w[0] + w[1] * x1
+y1 = w[0] + w[1] * x1 # Tính toán đầu ra dự đoán y1 cho x1 bằng cách sử dụng trọng số được huấn luyện w.
 print('Giá nhà cho 50m^2 là: ', y1)
 
 # Lưu w với numpy.save(), định dạng '.npy'
